ef86e1f9fb4cceec1f5ebfc4161461a9850ae715
Lucas Haddad <lucashaddad629@gmail.com>
2024-03-27 16:04:32 -0300

    adicionando classe endereco

--- src/main/java/com/project/screenmatch/model/Endereco.java
+++ src/main/java/com/project/screenmatch/model/Endereco.java
@@ -1,4 +1,20 @@
 package com.project.screenmatch.model;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Endereco {
+    @Value("${API_ENDPOINT}")
+    private static String ENDERECOBASE;
+
+    private String pesquisaDoUsuario;
+    @Value("${API_KEY}")
+    private static String APIKEY;
+    @Value("TEMPORADA")
+    private String TEMPORADAENDPOINT;
+
+
+
+    public static String montaEndereco(String pesquisaDoUsuario) {
+        return ENDERECOBASE + pesquisaDoUsuario.replace(" " , "+").toLowerCase() + APIKEY;
+    }
 }
