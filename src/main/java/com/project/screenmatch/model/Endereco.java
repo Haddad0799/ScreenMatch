6bcc1c0207844aba4cbb56e20c21099efbe4e852
Lucas Haddad <lucas.haddad0799@gmail.com>
2024-04-01 17:21:39 -0300

    atualizando variaveis de ambiente

--- src/main/java/com/project/screenmatch/model/Endereco.java
+++ src/main/java/com/project/screenmatch/model/Endereco.java
@@ -7,9 +7,10 @@ public class Endereco {
 
         public static String montaEnderecoTitulo(String pesquisaDoUsuario) {
             String ENDERECO_BASE = "https://www.omdbapi.com/?t=";
+            String API_KEY_ENDPOINT = "&apikey=";
             //String TEMPORADA_ENDPOINT = "&season=";
 
-            return ENDERECO_BASE + pesquisaDoUsuario.replace(" ", "+").toLowerCase() + System.getenv("OMDB_APIKEY");
+            return ENDERECO_BASE + pesquisaDoUsuario.replace(" ", "+").toLowerCase() + API_KEY_ENDPOINT + System.getenv("OMDB_APIKEY");
         }
     }
 
